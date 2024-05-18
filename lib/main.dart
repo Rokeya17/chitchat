92736745c840a6000d938b58ac7c79715cb7c343
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-05-18 23:03:36 +0600

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
 late Size mq;
 void main() {
+  _initializeFirebase();
   runApp(const ChitChat());
+}
+
+_initializeFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
